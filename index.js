250abb6537a5bd99a89aa4b10655a1aa3bdf8b9b
isabellagh <isabellagh88@gmail.com>
2021-03-28 23:50:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+// const cards = ["Ada", "Brendan", "Ali"]
+
+function writeCards( stringName, eventName) {
+    let thankYouCards = []
+    for (let i = 0; i < stringName.length; i++) {
+        thankYouCards.push(`Thank you, ${stringName[i]}, for the wonderful ${eventName} gift!` )
+    }
+    return thankYouCards
+}
+
+function countDown( startingNumber) {
+    while (startingNumber > 0 ) {
+        console.log(startingNumber);
+        startingNumber -= 1;
+    }
+    console.log(startingNumber);
+}
